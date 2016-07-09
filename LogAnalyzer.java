02e1ddd3f01e7f728c2a292d5a722a68773c275e
francisco2016 <otrayama@gmail.com>
2016-07-09 10:10:57 +0200

    -0070 modificado mt printHourlyCount() para que funcione con un bucle while.

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -44,11 +44,16 @@ public class LogAnalyzer
     public void printHourlyCounts()
     {
         System.out.println("Hr: Count");
-        for(int hour = 0; hour < hourCounts.length; hour++) {
+        //         for(int hour = 0; hour < hourCounts.length; hour++) {
+        //             System.out.println(hour + ": " + hourCounts[hour]);
+        //         }
+        int hour = 0;
+        while(hour < hourCounts.length){
             System.out.println(hour + ": " + hourCounts[hour]);
         }
+        hour ++;
     }
-    
+
     /**
      * Print the lines of data read by the LogfileReader
      */
